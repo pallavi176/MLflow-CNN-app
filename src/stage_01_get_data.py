96921327aa427548158997fed9eec550a22a8914
pallavi.saxena <saxena.pallavi7211@gmail.com>
2022-04-04 10:21:39 +0530

    stage 2 final changes

--- src/stage_01_get_data.py
+++ src/stage_01_get_data.py
@@ -40,8 +40,11 @@ def main(config_path):
 
     #Unzip operation
     unzip_data_dir = config["data"]["unzip_data_dir"]
-    create_directories([unzip_data_dir])
-    unzip_file(source=data_file_path, dest=unzip_data_dir)
+    if not os.path.exists(unzip_data_dir):
+        create_directories([unzip_data_dir])
+        unzip_file(source=data_file_path, dest=unzip_data_dir)
+    else:
+        logging.info(f"data already extracted")
 
     # Validating data
     validate_image(config)
